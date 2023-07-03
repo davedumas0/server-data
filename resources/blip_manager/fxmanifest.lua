0eb33cc21db83426d2ab9f39dd36e1d6981781cb
davedumas0 <davedumas0@gmail.com>
2023-07-03 09:02:06 -0700

    added a DB system and a blip system

---
+++ resources/blip_manager/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'chat GPT4'
+description 'Blip manager resource for my FiveM server'
+version '1.0.0'
+
+client_script 'client.lua'
---
+++ resources/couchdb_test/fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+--client_script 'client.lua' -- For client-side scripts
+server_script 'server.lua' -- For server-side scripts
